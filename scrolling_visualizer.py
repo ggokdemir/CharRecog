de2d5421cafc35d5d26a9fae208ee6e2696c853e
Gürkan Gökdemir <gurkangokdemir@outlook.com>
2019-05-10 17:42:09 +0300

    Create scrolling_visualizer.py

---
+++ scrolling_visualizer.py
@@ -0,0 +1,10 @@
+#%matplotlib tk
+
+#scrolling visualizer 
+#fig, ax = plt.subplots()
+#for x in range(raw_train_X.shape[0]):
+#    ax.clear()
+#    ax.imshow([i for i in 255 - raw_train_X[x].T], cmap='gray') #all data is flipped over, so we use T for transpose
+#    title = 'label %d = %s' %(raw_train_y[x],labels[raw_train_y[x]])
+#    ax.set_title(title,fontsize=20)
+#    plt.pause(1)
